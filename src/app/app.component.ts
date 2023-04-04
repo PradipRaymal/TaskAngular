c691a439c929d594d4e024394cc012e56ac5afff
Deep <pradip.raymal5511.com>
2023-04-05 01:15:48 +0530

    Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'UserRegistration';
+
+  title = 'User Registration Form';
+  genderList = ['Male', 'Female'];
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(255),
+    Validators.email
+  ]);
+  nameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(200)
+  ]);
+  genderFormControl = new FormControl('', [Validators.required]);
+  birthdateFormControl = new FormControl('', [Validators.required]);
+  workExperienceFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^\d+$/)
+  ]);
+
+  get age(): number {
+    const today = new Date();
+    const birthdateFormControlValue = this.birthdateFormControl.value;
+    if (!birthdateFormControlValue) {
+      return 0;
+    }
+    const birthdateString = birthdateFormControlValue.toString();
+    const birthdate = new Date(birthdateString.split('-').reverse().join('-'));
+    let age = today.getFullYear() - birthdate.getFullYear();
+    const month = today.getMonth() - birthdate.getMonth();
+    if (month < 0 || (month === 0 && today.getDate() < birthdate.getDate())) {
+      age--;
+    }
+    return age;
+  }
+
+  onSubmit() {
+    console.log('Form submitted successfully');
+    console.log('Email: ' + this.emailFormControl.value);
+    console.log('Name: ' + this.nameFormControl.value);
+    console.log('Gender: ' + this.genderFormControl.value);
+    console.log('Birthdate: ' + this.birthdateFormControl.value);
+    console.log('Age: ' + this.age);
+    console.log('Work Experience: ' + this.workExperienceFormControl.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
